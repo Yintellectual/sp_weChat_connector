eeb47bf5631ca6fdff2b101caf499b0f5a1c187e
Yu Chen <intellectual.y@gmail.com>
2018-04-05 17:29:12 +0800

    init

---
+++ src/main/java/com/sp/sp_weChat_connector/SpWeChatConnectorApplication.java
@@ -0,0 +1,12 @@
+package com.sp.sp_weChat_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWeChatConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpWeChatConnectorApplication.class, args);
+	}
+}
